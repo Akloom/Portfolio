631839bd04c0aedfd5ed50ab5604aba4207b83b4
Code-Monster1 <usupovaliakbar01@gmail.com>
2023-02-23 20:04:45 +0600

    about done

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,11 @@ language.addEventListener('click', () => {
   language.classList.toggle('is-active');
   languageContent.classList.toggle('is-active');
 });
+
+let question = document.querySelectorAll('.faq-content__info');
+
+    for (let i = 0; i < question.length; i++) {
+      question[i].addEventListener('click', () => {
+        question[i].classList.toggle('active');
+      })
+    }
