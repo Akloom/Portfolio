713c569f026c9d24c23231b97e2bdf23f9a0b34d
Code-Monster1 <usupovaliakbar01@gmail.com>
2024-02-07 12:15:40 +0600

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
-let hamburger = document.querySelector('.hamburger-menu');
-let navCollapse = document.querySelector('.header-nav__collapse');
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('is-active');
-  navCollapse.classList.toggle('is-active');
+let hamburger = document.querySelector(".hamburger-menu");
+let navCollapse = document.querySelector(".header-nav__collapse");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  navCollapse.classList.toggle("is-active");
 });
 
-let language = document.querySelector('.header-language__static');
-let languageContent = document.querySelector('.header-language__content');
-language.addEventListener('click', () => {
-  language.classList.toggle('is-active');
-  languageContent.classList.toggle('is-active');
+let language = document.querySelector(".header-language__static");
+let languageContent = document.querySelector(".header-language__content");
+language.addEventListener("click", () => {
+  language.classList.toggle("is-active");
+  languageContent.classList.toggle("is-active");
 });
 
-let question = document.querySelectorAll('.faq-content__info');
+// let question = document.querySelectorAll('.faq-content__info');
 
-    for (let i = 0; i < question.length; i++) {
-      question[i].addEventListener('click', () => {
-        question[i].classList.toggle('active');
-      })
-    }
+//     for (let i = 0; i < question.length; i++) {
+//       question[i].addEventListener('click', () => {
+//         question[i].classList.toggle('active');
+//       })
+//     }
